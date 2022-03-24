6f246908df5033c91931e21128938657f2c5332d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-03-24 21:34:41 +0100

    add bootstrap script

--- init.lua
+++ init.lua
@@ -1,10 +1,3 @@
 local MP = minetest.get_modpath(minetest.get_current_modname())
-local common = loadfile(MP.."/common.lua")()
-local crc32 = loadfile(MP.."/crc32.lua")()
-
-mtzip = {
-    crc32 = crc32,
-    common = common,
-    unzip = loadfile(MP.."/unzip.lua")(common, crc32),
-    zip = loadfile(MP.."/zip.lua")(common, crc32)
-}
+local bootstrap = loadfile(MP.."/bootstrap.lua")()
+mtzip = bootstrap(MP)
