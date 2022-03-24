dc951391a20e82315f42665de762fba20ed27dbf
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-03-24 21:30:21 +0100

    add real-world zip file

--- test/stages/stage1/init.lua
+++ test/stages/stage1/init.lua
@@ -1,22 +1,28 @@
 
-print("Stage 1: reading a zip file")
-
+print("Test: reading a simple zip file")
 local filename = minetest.get_modpath("mtzip") .. "/spec/out2.zip"
 local f = io.open(filename)
 local z = mtzip.unzip(f)
 local data = z:get("crc32.lua", true)
-print("len: " .. #data)
-if #data ~= 5996 then
-    error("content-length mismatch")
-end
+f:close()
+assert(#data == 5996, "content-length mismatch")
 
-print("Stage 2: creating a zip file")
+print("Test: reading a bx-exported zip file")
+filename = minetest.get_modpath("mtzip") .. "/spec/scifi_lamp_small.zip"
+f = io.open(filename)
+z = mtzip.unzip(f)
+assert(z:get_entry("schema.json") ~= nil, "schema.json not found")
+assert(z:get_entry("schemapart_0_0_0.json") ~= nil, "schemapart_0_0_0.json not found")
+data = z:get("mods.json", true)
+local mods = minetest.parse_json(data)
+assert(#mods == 1, "mods.json list count wrong")
+f:close()
 
+print("Test: creating a zip file")
 f = io.open(minetest.get_worldpath() .. "/stage1.zip", "w")
 z = mtzip.zip(f)
-
 z:add("test.txt", "abcdefghijklmnopqrstuvwxyz")
 z:close()
 
-
+-- done
 minetest.request_shutdown("success")
